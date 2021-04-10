bc62e0875b7f6395a264090250cc8400e2840ed4
Shehan Prasanna <shehanp068@gmail.com>
2021-04-10 12:15:31 +0530

    coneection is established

--- backend/index.js
+++ backend/index.js
@@ -3,5 +3,21 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/post.js';
 
+const app = express();
 
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors());
+
+app.use('/posts',postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://shehan:iaOnvM7u9epNVKRa@cluster0.lhv2q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
---
+++ backend/routes/post.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const router = express.Router();
+
+
+router.get('/',(req,res) => {
+
+    res.send('This Works');
+
+});
+
+export default router;
