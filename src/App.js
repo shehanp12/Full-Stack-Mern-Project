4c357814f5a9ce29a3292781cfa512e72c7f7a2e
Shehan Prasanna <shehanp068@gmail.com>
2021-04-11 19:27:15 +0530

    inital commit in frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import {AppBar, Container, Grid, Grow, Typography} from "@material-ui/core";
+import memories from 'images/memories.png';
+import Posts from '../src/components/Posts/Posts';
+import Form from '../src/components/Form/Form';
 const App = () => {
     return(
-        <div>
-            <h1>App</h1>
-        </div>
+      <Container maxWidth="lg">
+          <AppBar position="static" color="inherit">
+          <Typography variant="h2" align="center">Memories</Typography>
+          <img src={memories} alt="memories" height="60" />
+          </AppBar>
+          <Grow>
+              <Container>
+                  <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                      <Grid item xs={12} sm={7}>
+                          <Posts/>
+                      </Grid>
+                      <Grid item xs={12} sm={4}>
+                        <Form/>
+                      </Grid>
+
+                  </Grid>
+              </Container>
+          </Grow>
+
+      </Container>
+
+
     )
 
 }
