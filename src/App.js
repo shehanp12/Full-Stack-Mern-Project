c898c24f33f50b1125bebfcc285a349bb698ba92
Shehan Prasanna <shehanp068@gmail.com>
2021-04-12 10:42:24 +0530

    adding style components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {AppBar, Container, Grid, Grow, Typography} from "@material-ui/core";
-import memories from 'images/memories.png';
+import memories from './images/memories.png';
 import Posts from '../src/components/Posts/Posts';
 import Form from '../src/components/Form/Form';
+import useStyles from './styles';
+
 const App = () => {
+    const classes = useStyles();
     return(
       <Container maxWidth="lg">
-          <AppBar position="static" color="inherit">
-          <Typography variant="h2" align="center">Memories</Typography>
-          <img src={memories} alt="memories" height="60" />
+          <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography variant="h2" className={classes.heading} align="center">Memories</Typography>
+          <img src={memories} className={classes.image} alt="memories" height="60" />
           </AppBar>
-          <Grow>
+          <Grow in>
               <Container>
                   <Grid container justify="space-between" alignItems="stretch" spacing={3}>
                       <Grid item xs={12} sm={7}>
@@ -23,7 +26,6 @@ const App = () => {
                   </Grid>
               </Container>
           </Grow>
-
       </Container>
 
 
