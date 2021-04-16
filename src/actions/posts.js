3fa07323634c0fee63cb95002e60343ac744e84d
Shehan Prasanna <shehanp068@gmail.com>
2021-04-16 12:15:39 +0530

    connect with redux and database

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -14,3 +14,15 @@ import * as api from '../api';
    }
 
 }
+
+export const createPost = (post) => async (dispatch) => {
+
+     try {
+         const {data} = await api.createPost(post);
+
+         dispatch({type:'CREATE',payload:data});
+     }catch (e) {
+            console.log(e.message);
+     }
+
+}
